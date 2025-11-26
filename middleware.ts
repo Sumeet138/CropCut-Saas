3140bfd5450674547494fcd5b876750f8c957899
Sumeet_Gond <sumitv9009@gmail.com>
2025-11-26 21:21:12 +0530

    chore: update dependencies and add Prisma packages

--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,8 @@ const isPublicRoute = createRouteMatcher(["/", "/sign-in", "/sign-up", "/home"])
 
 const isPublicAPIRoute = createRouteMatcher(["/api/videos"])
 
-export default clerkMiddleware((auth, req) => {
-  const { userId } = auth()
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth()
   const currentUrl = new URL(req.url)
   const isAccesingDashBoard = currentUrl.pathname === "/home"
   const isApiRequest = currentUrl.pathname.startsWith("/api")
